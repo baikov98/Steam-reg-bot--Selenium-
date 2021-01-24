599a7b09a3e28690cc68b469c3dfc8d626cb84ff
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-24 21:32:00 +0500

    imports

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+#newmodal - класс у ждуна в стиме
+#MailListItem-dragWrap-2s  - класс контейнера письма
